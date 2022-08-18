081f43670e56cd627218e32160a1a673de0b917d
Edward Chisaka <edwardchisaka@Edwards-MacBook-Pro.local>
2022-08-17 22:43:28 -0700

    Complete Assignmet

---
+++ index.js
@@ -0,0 +1,10 @@
+//"has a function expression called divide"
+const divide = (num1=2000, num2=100) => num1 / num2;
+
+//has an arrow function called square
+const square = (parameter1 = 2) => parameter1 * parameter1;
+
+//"has an arrow function called add"
+const add = (a=3, b=4) => (a + b);
+
+
